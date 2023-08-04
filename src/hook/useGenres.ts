199c86edabb6c98b8c2c0c0825c7efd9f8fd562e
Kiana8181 <kianahashemi8@gmail.com>
2023-08-04 17:49:33 +0330

    refactoring: create a reusable apiClient

--- src/hook/useGenres.ts
+++ src/hook/useGenres.ts
@@ -1,21 +1,21 @@
-import { useQuery } from "@tanstack/react-query"
-import genres from "../data/genres"
-import apiClient, { FetchResponse } from "../services/api-client"
+import { useQuery } from "@tanstack/react-query";
+import genres from "../data/genres";
+import ApiClient, { FetchResponse } from "../services/api-client";
 
-export interface Genres{
-    id:number
-    name:string
-    image_background:string
+const apiClient = new ApiClient<Genres>("/genres");
+
+export interface Genres {
+  id: number;
+  name: string;
+  image_background: string;
 }
 
-const useGenres = () => useQuery({
-    queryKey:['genres'],
-    queryFn:()=> apiClient
-      .get<FetchResponse<Genres>>('/genres')
-      .then(res=>res.data),
-    staleTime:24*60*60*1000, //24h
-    initialData:{count:genres.length,results:genres}
-  })
+const useGenres = () =>
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: apiClient.getAll,
+    staleTime: 24 * 60 * 60 * 1000, //24h
+    initialData: { count: genres.length, results: genres },
+  });
 
-
-export default useGenres
+export default useGenres;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,29 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
-    count: number;
-    results: T[];
+  count: number;
+  results: T[];
+}
+
+const axiosInstance = axios.create({
+  baseURL: "https://api.rawg.io/apim",
+  params: {
+    key: "a97d04a29a7a40f1bfb4ccbb371ae75e",
+  },
+});
+
+class ApiClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
   }
 
-export default axios.create({
-    baseURL:"https://api.rawg.io/apim",
-    params:{
-        key:"a97d04a29a7a40f1bfb4ccbb371ae75e"
-    }
-})
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+}
+
+export default ApiClient;
