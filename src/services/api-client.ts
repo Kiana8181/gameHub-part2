54e6e12e616a85b31b72bc5b250931656e11058a
Kiana8181 <kianahashemi8@gmail.com>
2023-09-25 10:05:18 +0330

    build game screenshots

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -7,7 +7,7 @@ export interface FetchResponse<T> {
 }
 
 const axiosInstance = axios.create({
-  baseURL: "https://api.rawg.io/apim",
+  baseURL: "https://api.rawg.io/api",
   params: {
     key: "a97d04a29a7a40f1bfb4ccbb371ae75e",
   },
